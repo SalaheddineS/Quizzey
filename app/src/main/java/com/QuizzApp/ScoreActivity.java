27cc239451093e1bbc10d73e891f00da899e1e01
Salaheddine <Salaheddinemorchid1@gmail.com>
2023-03-29 18:12:52 +0000

    Added Restart option + corrected UI

--- app/src/main/java/com/QuizzApp/ScoreActivity.java
+++ app/src/main/java/com/QuizzApp/ScoreActivity.java
@@ -1,6 +1,8 @@
 package com.QuizzApp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,14 +10,23 @@ import androidx.appcompat.app.AppCompatActivity;
 public class ScoreActivity extends AppCompatActivity {
 
     private TextView displayScore;
-
+    private ImageView restart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
         String score = getIntent().getStringExtra("score");
         displayScore = findViewById(R.id.textView4);
+        restart =findViewById(R.id.imageView6);
         displayScore.setText(score);
+        restart.setOnClickListener(v -> {
+            try {
+                startActivity(new Intent(ScoreActivity.this,LanguageActivity.class));
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 
 }
