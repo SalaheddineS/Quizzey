f396e1474ce49618a66940e85c7d07ebe365fd68
Salaheddine <Salaheddinemorchid1@gmail.com>
2023-03-29 19:48:07 +0000

    Added Firestore compatibility with register to get the users data

--- app/src/main/java/com/QuizzApp/RegisterActivity.java
+++ app/src/main/java/com/QuizzApp/RegisterActivity.java
@@ -11,6 +11,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
@@ -18,7 +19,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.ktx.Firebase;
+import com.google.type.DateTime;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
     Button goBackLogin;
@@ -26,15 +33,24 @@ public class RegisterActivity extends AppCompatActivity {
     Button newPassword;
     Button ConfirmedPassword;
     Button Submit;
+    Button adresse;
+    Button name;
+    RadioButton Homme;
+    RadioButton Femme;
     FirebaseAuth mAuth ;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
         goBackLogin = findViewById(R.id.button5);
         newEmail = findViewById(R.id.login);
         newPassword =findViewById(R.id.password);
         Submit = findViewById(R.id.button);
         ConfirmedPassword = findViewById(R.id.cpassword);
+        adresse = findViewById(R.id.adresse);
+        name = findViewById(R.id.name);
+        Homme = findViewById(R.id.radio_male);
+        Femme = findViewById(R.id.radio_female);
         newEmail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -137,15 +153,39 @@ public class RegisterActivity extends AppCompatActivity {
                 String email = newEmail.getText().toString();
                 String password = newPassword.getText().toString();
                 String cpassword = ConfirmedPassword.getText().toString();
-
+                String nameText = name.getText().toString();
+                String adresseText = adresse.getText().toString();
+                String sexe = "";
+                if (Homme.isChecked()) {
+                    sexe = "Homme";
+                } else if (Femme.isChecked()) {
+                    sexe = "Femme";
+                }
+                HashMap<String, Object> user = new HashMap<>();
+                user.put("name", nameText);
+                user.put("email", email);
+                user.put("adresse", adresseText);
+                user.put("sexe", sexe);
+                user.put("creationDate", LocalDateTime.now());
                 if (password.equals(cpassword) && !TextUtils.isEmpty(email)) {
+
                     mAuth = FirebaseAuth.getInstance();
                     mAuth.createUserWithEmailAndPassword(email, password)
                             .addOnCompleteListener(task -> {
                                 if (task.isSuccessful()) {
-                                    Toast.makeText(RegisterActivity.this, "User created", Toast.LENGTH_SHORT).show();
-                                    Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
-                                    startActivity(intent);
+                                    db.collection("users").add(user)
+                                            .addOnSuccessListener(documentReference -> Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId()))
+                                            .addOnFailureListener(e -> Log.w(TAG, "Error adding document", e)).addOnCompleteListener(tsk -> {
+                                                if (tsk.isSuccessful()) {
+                                                    Toast.makeText(RegisterActivity.this, "User created", Toast.LENGTH_SHORT).show();
+                                                    Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                                                    startActivity(intent);
+                                                } else {
+                                                    Toast.makeText(RegisterActivity.this, "User not created", Toast.LENGTH_SHORT).show();
+                                                }
+                                            });
+
+
                                 } else {
                                     Toast.makeText(RegisterActivity.this, "User not created", Toast.LENGTH_SHORT).show();
                                 }
@@ -155,8 +195,9 @@ public class RegisterActivity extends AppCompatActivity {
                                 Toast.makeText(RegisterActivity.this, "Error creating user", Toast.LENGTH_SHORT).show();
                             });
                 } else {
-                    Toast.makeText(RegisterActivity.this, "Passwords do not match or email is empty", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(RegisterActivity.this, "Passwords do not match or one of the attributes is empty", Toast.LENGTH_SHORT).show();
                 }
+
             }
 
         });
